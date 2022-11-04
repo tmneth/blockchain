c2ac4b9bf5fceb95e9f1727fccca0b76c2680b8a
tmneth <80415416+tmneth@users.noreply.github.com>
2022-11-04 18:17:32 +0200

    Properly update shared pool and users' balances

--- src/utils.cpp
+++ src/utils.cpp
@@ -10,7 +10,7 @@ double genRandAmount() {
 
 void genUsers(std::vector<User> &users) {
 
-    for (int i = 1; i <= USER_NUM; ++i) {
+    for (int i = 0; i < USER_NUM; ++i) {
         User user("user" + std::to_string(i), genRandAmount());
 
         users.push_back(user);
@@ -23,6 +23,7 @@ void genPool(std::vector<User> &users, std::vector<Transaction> &pool) {
     std::random_device device;
     std::mt19937 mt(device());
     std::uniform_int_distribution<int> seed(0, users.size() - 1);
+
 
     for (int i = 0; i < TRANSACTION_NUM; ++i) {
         double amount = genRandAmount();
@@ -46,16 +47,6 @@ void genPool(std::vector<User> &users, std::vector<Transaction> &pool) {
 
 }
 
-void purgeTransactions(std::vector<Transaction> newPool, std::vector<Transaction> &pool) {
-
-    for (Transaction &tx: newPool) {
-        auto it = std::find_if(pool.begin(), pool.end(),
-                               [&tx](const Transaction &t) { return t.getId() == tx.getId(); });
-        pool.erase(it);
-    }
-
-}
-
 int findUser(std::string publicKey, std::vector<User> &users) {
 
     auto it = find_if(users.begin(), users.end(),
@@ -66,50 +57,46 @@ int findUser(std::string publicKey, std::vector<User> &users) {
 
 }
 
-std::vector<Transaction> processTransactions(std::vector<Transaction> &pool, std::vector<User> &users) {
+std::vector<Transaction> selectTransactions(std::vector<Transaction> &pool, std::vector<User> &users) {
 
-    if (pool.size() < REDUCED_TRANSACTION_NUM) {
-        return pool;
-    } else {
+    std::vector<Transaction> newPool;
+    MYSHA hash;
 
-        std::vector<Transaction> newPool;
-        MYSHA hash;
-        std::vector<int> randInd;
+    std::random_device device;
+    std::mt19937 mt(device());
 
-        for (int i = 0; i < REDUCED_TRANSACTION_NUM; ++i)
-            randInd.push_back(i);
+    while (newPool.size() < REDUCED_TRANSACTION_NUM && !pool.empty()) {
 
-        std::shuffle(randInd.begin(), randInd.end(), std::mt19937(std::random_device()()));
+        std::uniform_int_distribution<int> seed(0, pool.size() - 1);
 
-        for (int i = 0; i < REDUCED_TRANSACTION_NUM; ++i) {
+        int index = seed(mt);
 
-            int index = randInd[i];
+        int senderId = findUser(pool[index].getSender(), users);
+        int recipientId = findUser(pool[index].getRecipient(), users);
 
-            int senderId = findUser(pool[index].getSender(), users);
-            int recipientId = findUser(pool[index].getRecipient(), users);
+        double txAmount = pool[index].getAmount();
 
-            double txAmount = pool[index].getAmount();
+        if (users[senderId].getBalance() >= txAmount
+            &&
+            hash(users[senderId].getPublicKey()
+                 + users[recipientId].getPublicKey()
+                 + std::to_string(pool[index].getAmount())
+                 + std::to_string(pool[index].getTimestamp()))
+            == pool[index].getHash()) {
 
-            if (users[senderId].getBalance() >= txAmount &&
-                hash(users[senderId].getPublicKey()
-                     + users[recipientId].getPublicKey()
-                     + std::to_string(pool[index].getAmount())
-                     + std::to_string(pool[index].getTimestamp()))
-                == pool[index].getHash()) {
+            newPool.push_back(pool.at(index));
 
-                newPool.push_back(pool[index]);
+            users[senderId].setBalance(users[senderId].getBalance() - txAmount);
 
-                users[senderId].setBalance(users[senderId].getBalance() - txAmount);
-
-                users[recipientId].setBalance(users[recipientId].getBalance() + txAmount);
-
-            } else {
-                pool.erase(pool.begin() + index);
-            }
+            users[recipientId].setBalance(users[recipientId].getBalance() + txAmount);
 
         }
-        return newPool;
+
+        pool.erase(pool.begin() + index);
+
     }
+
+    return newPool;
 }
 
 void initBlockchain(Blockchain &chain, std::vector<Transaction> pool, std::vector<User> &users) {
@@ -120,42 +107,51 @@ void initBlockchain(Blockchain &chain, std::vector<Transaction> pool, std::vecto
     genPool(users, pool);
     std::vector<User> oldUsers(users);
 
+    std::vector<User> tempUsers(users);
+    std::vector<Transaction> tempPool(pool);
+
+
     for (int i = 0; !pool.empty(); i++) {
-        std::vector<Transaction> newPool;
-        std::vector<Block> blocks;
+
         char blockName = 'a';
         int maxNonce = 10000;
+        bool isMined = false;
 
-        for (int j = 0; j < 5; ++j)
+        while (!isMined) {
 
-            blocks.emplace_back(chain.getPrevHash(), processTransactions(pool, users));
+            for (int j = 0; j < CANDIDATES; j++) {
 
-        for (Block block: blocks) {
+                Block block(chain.getPrevHash(), selectTransactions(tempPool, tempUsers));
 
-            if (block.mine(maxNonce)) {
+                if (block.mine(maxNonce)) {
+                    pool = tempPool;
+                    users = tempUsers;
 
-                purgeTransactions(block.getData(), pool);
+                    std::ofstream fout("blocks/block" + std::to_string(i) + ".txt");
+                    fout << block;
+                    fout.close();
 
-                std::ofstream fout("blocks/block" + std::to_string(i) + ".txt");
-                fout << block;
-                fout.close();
+                    chain.appendBlock(block);
+                    std::cout << "Mined block " << blockName << std::endl;
 
-                chain.appendBlock(block);
-                std::cout << "Mined block " << blockName << std::endl;
-                break;
-
-            } else {
-
-                blockName++;
+                    isMined = true;
+                    break;
+                } else {
+                    tempPool = pool;
+                    tempUsers = users;
+                    blockName++;
+                    continue;
+                }
+            }
+            if (!isMined) {
                 maxNonce *= 2;
-
             }
         }
     }
-
-    std::cout << "User details:" << std::endl;
-    for (int i = 0; i < users.size(); ++i) {
-        std::cout << oldUsers[i].getName() << " " << oldUsers[i].getPublicKey() << " | $" << oldUsers[i].getBalance()
-                  << " -> $" << users[i].getBalance() << std::endl;
-    }
+//
+//    std::cout << "User details:" << std::endl;
+//    for (int i = 0; i < users.size(); ++i) {
+//        std::cout << oldUsers[i].getName() << " " << oldUsers[i].getPublicKey() << " | $" << oldUsers[i].getBalance()
+//                  << " -> $" << users[i].getBalance() << std::endl;
+//    }
 }
